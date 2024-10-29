d12d0dbdd15417f53bf52431c3aefb2806802604
Daryl John Tadeo <daryltadeo@Daryls-MacBook-Pro.local>
2024-10-29 23:06:46 +0800

    NameSpaces

--- dotnet-course-code/HelloWorld/Program.cs
+++ dotnet-course-code/HelloWorld/Program.cs
@@ -1,17 +1,8 @@
 ﻿using System;
+using HelloWorld.Models;
 
 namespace HelloWorld
 {
-    // Models
-    public class Computer {
-        public string? Motherboard { get; set; } = "";
-        public int CPUCores { get; set; }
-        public bool HasWifi { get; set; }
-        public bool HasLTE { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public decimal Price { get; set; }
-        public string? VideoCard { get; set; } = "";
-    }
     internal class Program
     {
         static void Main(string[] args)
